8a0bdff37b2a92bed20c817d3da25e9300e7a0e4
prabeshlama <prabeshlama0@gmail.com>
2021-06-11 00:25:26 -0500

    Committing the FirstGItHubProject

---
+++ FirstGitHubProject/src/FirstGitHubProject.java
@@ -0,0 +1,7 @@
+
+public class FirstGitHubProject {
+   public static void main(String[] args) {
+	System.out.println("THis is the example to show how to fetch the java program from eclpse to the gitHub ");
+	
+}
+}
---
+++ FirstGitHubProject/src/SecondGItHubProject.java
@@ -0,0 +1,6 @@
+
+public class SecondGItHubProject {
+public static void main(String[] args) {
+	System.out.println("Fetching the programs from eclipse to the GitHub");
+}
+}
